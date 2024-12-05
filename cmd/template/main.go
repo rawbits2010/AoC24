12577f6a0cc49b67c30b1e36bdcdab3629ce2a62
rawbits <rawbits2010@gmail.com>
2024-12-05 17:08:39 +0100

    + AoC template copy

---
+++ cmd/template/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/rawbits2010/AoC23/internal/inputhandler"
+)
+
+func main() {
+
+	lines := inputhandler.ReadInput()
+
+	var result int
+	// do something with input lines
+	_ = lines
+
+	fmt.Printf("Result: %d", result)
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/rawbits2010/AoC23
+
+go 1.21.4
+
+require (
+	golang.org/x/exp v0.0.0-20231127185646-65229373498e // indirect
+	golang.org/x/sys v0.15.0
+)
